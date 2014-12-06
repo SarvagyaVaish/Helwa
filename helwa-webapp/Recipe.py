c54e58de014353585e7edab30d099c1e22f4ee35
Ahmed Ismail <ahmedismail91@gmail.com>
2014-12-06 14:47:43 -0500

    Add Recipe.py to scrape directions, ingredients
    Currently tested with foodnetwork.com recipes

---
+++ helwa-webapp/Recipe.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup, element, NavigableString
+import urllib2, re
+
+class Recipe:
+
+	soup = BeautifulSoup()
+	nutrition = ""
+
+	def __init__(self, url):
+		self.url = url
+		page = urllib2.urlopen(self.url).read()
+		Recipe.soup = BeautifulSoup(page)
+	
+	def strip_tags(self, snippet, invalid_tags):
+	    for tag in snippet:
+	        if tag.name in invalid_tags:
+	            s = ""
+	            for c in tag.contents:
+	                if not isinstance(c, NavigableString):
+	                    c = strip_tags(unicode(c), invalid_tags)
+	                s += unicode(c)
+	            tag.replaceWith(s)
+	    return snippet
+
+
+	def getTitle(self):
+		return  Recipe.soup.find('h1', attrs={"itemprop": "name"}).string
+		
+	# Scrape a Recipe's Ingredients
+	def ScrapeIngredients(self):
+		ingredients = Recipe.soup.findAll('li', attrs={"itemprop": "ingredients"}, text=True)
+		arr = []
+		for ingredient in ingredients:
+			arr.append(ingredient.string)
+		return arr
+
+	# Scape a Recipe's Directions - for foodnetwork.com
+	def ScrapeDirections(self):
+		# Must use the 4th element in the resultant array for foodnetwork's setup
+		directions = Recipe.soup.find(attrs={"itemprop": "recipeInstructions"}).findAll('p')
+		arr = []
+		for direction in directions:
+			# Get rid of the anchor tags
+			direction = self.strip_tags(direction, ['a']).getText()
+			# Save Recipe's nutritional Information, if available
+			if "Calories" in direction:
+				self.nutrition = direction
+			# Remove misc info at the bottom of directions section
+			elif "Food Network" not in direction and "Photograph" not in direction and len(direction.split()) > 2:
+				# Split sentences
+				m = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', direction)
+				for x in m:
+					arr.append(x)
+
+		return arr
+
+	# Return Recipe Nutritional Info
+	def getNutrition(self):
+		return  self.nutrition
+
+
+################# TESTING #################################
+test1 = Recipe('http://www.foodnetwork.com/recipes/food-network-kitchens/glazed-salmon-with-spiced-carrots-recipe.html')
+print test1.getTitle()
+print "Ingredients"
+for i in test1.ScrapeIngredients():
+	print i
+print
+print "Directions"
+for i in test1.ScrapeDirections():
+	print i
+if test1.getNutrition():
+	print "Nutritional Info"
+	print test1.getNutrition()
+
+# print len(test1.ScrapeDirections())
+
+print
+print
+
+test2 = Recipe('http://www.foodnetwork.com/recipes/food-network-kitchens/classic-glazed-ham-recipe.html')
+print test2.getTitle()
+print "Ingredients"
+for i in test2.ScrapeIngredients():
+	print i
+print
+print "Directions"
+for i in test2.ScrapeDirections():
+	print i
+if test2.getNutrition():
+	print "Nutritional Info"
+	print test2.getNutrition()
+
--- sandbox/bsouptest.py
+++ sandbox/bsouptest.py
@@ -1,9 +1,59 @@
-from bs4 import BeautifulSoup
-import urllib2
+from bs4 import BeautifulSoup, element, NavigableString
+import urllib2, re
 
-page = urllib2.urlopen('http://www.foodnetwork.com/recipes/food-network-kitchens/spanish-style-noodles-with-chicken-and-sausage-recipe.html').read()
+page = urllib2.urlopen('http://www.foodnetwork.com/recipes/food-network-kitchens/glazed-salmon-with-spiced-carrots-recipe.html').read()
+# page = urllib2.urlopen('http://www.foodnetwork.com/recipes/food-network-kitchens/classic-glazed-ham-recipe.html').read()
 soup = BeautifulSoup(page)
 
-soup.prettify()
-for anchor in soup.findAll('a', href=True):
-    print anchor['href']
+# print soup.findAll(attrs={"itemprop": "recipeInstructions"})
+def strip_tags(snippet, invalid_tags):
+
+    # for tag in snippet.findAll(True):
+    for tag in snippet:
+        if tag.name in invalid_tags:
+            s = ""
+
+            for c in tag.contents:
+                if not isinstance(c, NavigableString):
+                    c = strip_tags(unicode(c), invalid_tags)
+                s += unicode(c)
+
+            tag.replaceWith(s)
+
+    return snippet
+
+# x = soup.find(attrs={"itemprop": "recipeInstructions"}).contents[3]
+# strip_tags(x, ['a'])
+# print x.getText()
+
+# print type(soup.findAll(attrs={"itemprop": "recipeInstructions"})[0])
+
+# print len(soup.findAll(attrs={"itemprop": "recipeInstructions"}))
+
+# Must use the 4th element in the resultant array for foodnetwork's setup
+directions = soup.find(attrs={"itemprop": "recipeInstructions"}).findAll('p')
+# print directions
+# strip_tags(directions, ['a'])
+
+nutrition = ""
+arr = []
+
+for direction in directions:
+	i = strip_tags(direction, ['a']).getText()
+	if "Calories" in i:
+		nutrition = i
+	elif "Food Network" not in i and "Photograph" not in i:
+		m = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', i)
+		print type(m)
+		for x in m:
+			arr.append(x)
+# print soup.find(attrs={"itemprop": "recipeInstructions"}).contents
+# print directions.getText()
+
+for i in arr:
+	print i
+
+print len(arr)
+# print nutrition
+
+# print soup.find(attrs={"itemprop": "recipeInstructions"}).findAll('p')
