272b688059f00cbbc9bf7dbe002b2dd02296f710
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2014-12-06 04:00:59 -0500

    Setting up basic webapp
    - Working home page with form to submit url
    - Recipe Engine receives the url and displays it on the screen

---
+++ helwa-webapp/helwa.py
@@ -0,0 +1,60 @@
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+import logging
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+
+# [START HomePage]
+class HomePage(webapp2.RequestHandler):
+
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('HomePage.html')
+        self.response.write(template.render())
+# [END HomePage]
+
+
+# [START RecipeEnginePage]
+class RecipeEnginePage(webapp2.RequestHandler):
+
+    def get(self):
+        query_params = {'Error': "no-url-found"}
+        self.redirect('/?' + urllib.urlencode(query_params))
+
+    def post(self):
+        recipeUrl = self.request.get('recipe_url')
+
+        logging.info("Url Recived:  " + recipeUrl)
+
+
+        ################################
+        ##        Fetch Recipe        ##
+        ################################
+
+
+        template_values = {
+            'RecipeUrl': recipeUrl
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('RecipeEnginePage.html')
+        self.response.write(template.render(template_values))
+# [END RecipeEnginePage]
+
+
+application = webapp2.WSGIApplication([
+    ('/', HomePage),
+    ('/RecipeEngine', RecipeEnginePage),
+], debug=True)
