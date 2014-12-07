9b98a2c631cca29ffefeab9f203c0e57d207c641
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2014-12-06 22:03:02 -0500

    Create a DynamicHtmlGenerator class to populate the website with ingredients and directions

---
+++ helwa-webapp/js/DynamicHtmlGenerator.js
@@ -0,0 +1,53 @@
+var DynamicHtmlGenerator = (function () {
+
+
+
+    // 
+    // Template Code For Reference
+    // 
+    // var myPrivateVar, myPrivateMethod;
+    //
+    // // A private counter variable
+    // myPrivateVar = 0;
+    //
+    // // A private function which logs any arguments
+    // myPrivateMethod = function( foo ) {
+    //     console.log( foo );
+    // };
+
+    return {
+
+        CreateIngredientList: function( ingredients ) {
+
+            // TODO: Use document.writes / templates to populate 
+            //       a list of ingredients on the webpage
+
+        }, 
+
+
+        CreateDirectionList: function( directions ) {
+
+            // TODO: Use document.writes / templates to populate 
+            //       a list of directions on the webpage
+
+        }
+
+
+        // 
+        //  Template Code For Reference
+        //        
+        // // A public variable
+        // myPublicVar: "foo",
+        //
+        // // A public function utilizing privates
+        // myPublicFunction: function( bar ) {
+        //
+        //   // Increment our private counter
+        //   myPrivateVar++;
+        //
+        //   // Call our private method using bar
+        //   myPrivateMethod( bar );
+        // }
+    };
+
+})();
