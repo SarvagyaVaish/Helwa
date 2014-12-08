49de940c30abec2c50df73dc668322a2b3fd0e3f
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2014-12-07 19:36:35 -0500

    Coding up the AssociateIngredientsWithDirections method

--- helwa-webapp/Recipe.py
+++ helwa-webapp/Recipe.py
@@ -106,7 +106,7 @@ class Recipe:
         #         "Content-Type": "application/x-www-form-urlencoded"
         #     })
 
-        result = '{"result": "Preheat/NNP the/DT broiler/NN ./. Lightly/RB oil/NN the/DT broiler/NN pan/NN ./. Toss/NNP the/DT carrots/NNS with/IN 1/CD tablespoon/NN olive/JJ oil/NN in/IN a/DT bowl/NN ,/, then/RB spread/VB out/RP on/IN one/CD side/NN of/IN the/DT pan/NN ./. Broil/NNP until/IN the/DT carrots/NNS begin/VBP to/TO soften/VB ,/, 2/CD to/TO 3/CD minutes.Meanwhile/JJ ,/, whisk/NN 2/CD tablespoons/NNS olive/JJ oil/NN ,/, the/DT honey/NN ,/, mustard/NN and/CC 1/CD tablespoon/NN lime/NN juice/NN in/IN a/DT small/JJ bowl/NN ./. Brush/NNP the/DT tops/NNS and/CC sides/NNS of/IN the/DT salmon/NN with/IN the/DT glaze/NN ./. Put/NNP the/DT salmon/NN ,/, skin-side/JJ down/IN ,/, on/IN the/DT other/JJ side/NN of/IN the/DT broiler/NN pan/NN next/IN to/TO the/DT carrots/NNS and/CC season/NN with/IN salt/NN and/CC pepper/NN ./. Broil/NNP until/IN the/DT salmon/NN is/VBZ golden/VBN brown/RP and/CC just/RB cooked/VBD through/IN and/CC the/DT carrots/NNS are/VBP crisp-tender/JJ ,/, 5/CD to/TO 7/CD minutes.Whisk/JJ the/DT remaining/VBG 1/CD tablespoon/NN each/DT olive/JJ oil/NN and/CC lime/NN juice/NN ,/, the/DT coriander/NN ,/, cumin/NN ,/, cinnamon/NN ,/, mint/NN and/CC almonds/NNS in/IN a/DT bowl/NN ./. Add/NNP the/DT carrots/NNS and/CC toss/NN to/TO combine/VB ;/: season/NN with/IN salt/NN and/CC pepper/NN ./. Serve/NNP the/DT salmon/NN with/IN the/DT carrots/NNS and/CC lime/VB wedges/NNS ./."}'
+        result = '{"result": "Preheat/NNP the/DT broiler/NN ./. Lightly/RB oil/NN the/DT broiler/NN pan/NN ./. Toss/NNP the/DT carrots/NNS with/IN 1/CD tablespoon/NN olive/JJ oil/NN in/IN a/DT bowl/NN ,/, then/RB spread/VB out/RP on/IN one/CD side/NN of/IN the/DT pan/NN ./. Broil/NNP until/IN the/DT carrots/NNS begin/VBP to/TO soften/VB ,/, 2/CD to/TO 3/CD minutes.Meanwhile/JJ ,/, whisk/NN 2/CD tablespoons/NNS olive/JJ oil/NN ,/, the/DT honey/NN ,/, mustard/NN and/CC 1/CD tablespoon/NN lime/NN juice/NN in/IN a/DT small/JJ bowl/NN ./. Brush/NNP the/DT tops/NNS and/CC sides/NNS of/IN the/DT salmon/NN with/IN the/DT glaze/NN ./. Put/NNP the/DT salmon/NN ,/, skin-side/JJ down/IN ,/, on/IN the/DT other/JJ side/NN of/IN the/DT broiler/NN pan/NN next/IN to/TO the/DT carrots/NNS and/CC season/NN with/IN salt/NN and/CC pepper/NN ./. Broil/NNP until/IN the/DT salmon/NN is/VBZ golden/VBN brown/RP and/CC just/RB cooked/VBD through/IN and/CC the/DT carrots/NNS are/VBP crisp-tender/JJ ,/, 5/CD to/TO 7/CD minutes. Whisk/JJ the/DT remaining/VBG 1/CD tablespoon/NN each/DT olive/JJ oil/NN and/CC lime/NN juice/NN ,/, the/DT coriander/NN ,/, cumin/NN ,/, cinnamon/NN ,/, mint/NN and/CC almonds/NNS in/IN a/DT bowl/NN ./. Add/NNP the/DT carrots/NNS and/CC toss/NN to/TO combine/VB ;/: season/NN with/IN salt/NN and/CC pepper/NN ./. Serve/NNP the/DT salmon/NN with/IN the/DT carrots/NNS and/CC lime/VB wedges/NNS ./."}'
         resultDict = ast.literal_eval(result)
         
         self.m_TaggedDirectionString = resultDict["result"]
@@ -143,9 +143,27 @@ class Recipe:
             directionDict['action-nodes'] = []
 
             matchResultsIt = self.m_VerbPattern.finditer(directionString)
+            foundVerbFlag = False
             for matchResult in matchResultsIt:
+                foundVerbFlag = True
                 verb = matchResult.group(1)
-                newActionNode = { "verb": verb }
+                newActionNode = { 
+                    "verb": verb,
+                    "ingredientIds": []
+                }
+                directionDict['action-nodes'].append(newActionNode)
+
+            # If no verb was found, use the first word of the sentence
+            if not foundVerbFlag:
+                verb = "Unknown"
+                matchResult = re.match(r"([a-zA-Z]+)\/[A-Z]{2,3}", directionString)
+                if matchResult:
+                    verb = matchResult.group(1)
+                
+                newActionNode = { 
+                    "verb": verb,
+                    "ingredientIds": []
+                }
                 directionDict['action-nodes'].append(newActionNode)
 
             self.m_SplitDirections[i] = directionDict
@@ -155,6 +173,51 @@ class Recipe:
 
     # Associate ingredients with directions
     def AssociateIngredientsWithDirections(self):
+
+        # Create a map from ingredient ids to ingredient names (words)
+        ingredientMap = {}
+        for ingredient in self.m_StructuredIngredients:
+            ingredientMap[ingredient["id"]] = []
+
+            ingredientString = ingredient["name"]
+            ingredientWords = re.split(r" |,|\.|'", ingredientString)
+            for word in ingredientWords:
+                if len(word) > 3:
+                    ingredientMap[ingredient["id"]].append(word.lower())
+
+        logging.info(ingredientMap)
+
+        # Iterate over each direction prettySentence and try to find ingredient words
+        for i in range(len(self.m_SplitDirections)):
+            directionString = self.m_SplitDirections[i]["prettySentence"]
+            directionString = directionString.lower()
+
+            for ingredientId, ingredientWords in ingredientMap.iteritems():
+                for ingredientWord in ingredientWords:
+
+                    # try to find the word in the direction string
+                    ingredientWordLoc = directionString.find(ingredientWord)
+                    if ingredientWordLoc >= 0:
+                        # find locations of the verbs in the current direction string
+                        actionNodeLocations = []
+                        for actionNode in self.m_SplitDirections[i]["action-nodes"]:
+                            actionNodeLocations.append(directionString.find(actionNode['verb']))
+
+                        # find verb that is closest to ingredient word
+                        minDist = 999999
+                        minId = -1
+                        for locIndex in range(len(actionNodeLocations)):
+                            if abs(actionNodeLocations[locIndex] - ingredientWordLoc) < minDist:
+                                minDist = abs(actionNodeLocations[locIndex] - ingredientWordLoc)
+                                minId = locIndex
+
+                        # associate this ingredient with the minId action-node
+                        self.m_SplitDirections[i]["action-nodes"][minId]['ingredientIds'].append(ingredientId)
+                        break # to avoid duplicates
+
+            logging.info("####    Associated Ingredients Results    ####")
+            logging.info(self.m_SplitDirections[i])
+
         return
 
 
